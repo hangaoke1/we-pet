48e8a464b7d941e288f9edae29e5203bba25d641
hangaoke1 <188869009@qq.com>
2020-02-23 21:50:53 +0800

    feat: update

--- src/pages/subscribe/index.js
+++ src/pages/subscribe/index.js
@@ -1,14 +1,55 @@
 import Taro, { Component } from '@tarojs/taro'
 import { connect } from '@tarojs/redux'
-import { View, Swiper, SwiperItem, Image } from '@tarojs/components'
+import classNames from 'classnames'
+import { View, Swiper, SwiperItem, Image, ScrollView, Block, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
 import apiHome from '@/api/home'
 import Iconfont from '@/components/Iconfont'
 import makePhoneCall from '@/lib/makePhoneCall'
 import config from '@/config'
 import _ from '@/lib/lodash'
 import { getPet } from '@/actions/pet'
+import dayjs from 'dayjs'
 
 import './index.less'
+
+function isDisabled (dateFmt, time) {
+  const current = dayjs()
+  const d = dayjs(`${dateFmt} ${time}`)
+  return d.isBefore(current)
+}
+
+function genDate () {
+  const arr = [
+    dayjs(),
+    dayjs().add(1, 'day'),
+    dayjs().add(2, 'day'),
+    dayjs().add(3, 'day'),
+    dayjs().add(4, 'day'),
+    dayjs().add(5, 'day'),
+    dayjs().add(6, 'day'),
+    dayjs().add(7, 'day'),
+    dayjs().add(8, 'day')
+  ]
+  const weekMap = {
+    0: '周日',
+    1: '周一',
+    2: '周二',
+    3: '周三',
+    4: '周四',
+    5: '周五',
+    6: '周六'
+  }
+  const newArray = arr.map((item) => {
+    return {
+      date: item.format('MM.DD'),
+      dateFmt: item.format('YYYY-MM-DD'),
+      week: weekMap[item.day()],
+      value: item
+    }
+  })
+  return newArray
+}
 
 @connect(
   ({ pet }) => ({
@@ -22,11 +63,36 @@ class index extends Component {
   }
 
   state = {
+    storeId: 1,
     petId: '',
-    banners: []
+    service: '洗澡',
+    date: '',
+    dateFmt: '',
+    timeInt: '',
+    time: '',
+    banners: [],
+    // 洗澡、spa、美容、洁齿
+    serviceList: [
+      { name: '洗澡', icon: '' },
+      { name: 'spa', icon: '' },
+      { name: '美容', icon: '' },
+      { name: '洁齿', icon: '' }
+    ],
+    dateList: [],
+    timeList: [
+      [ '10:00', '11:00', '12:00', '13:00' ],
+      [ '14:00', '15:00', '16:00', '17:00' ],
+      [ '18:00', '19:00', '20:00', '21:00' ]
+    ]
   }
 
   componentWillMount () {
+    const dateList = genDate()
+    this.setState({
+      dateList,
+      date: dateList[0].date,
+      dateFmt: dateList[0].dateFmt
+    })
     apiHome
       .queryBanners()
       .then((res) => {
@@ -76,6 +142,65 @@ class index extends Component {
     })
   }
 
+  selectedService = (item) => {
+    this.setState({
+      service: item.name
+    })
+  }
+
+  chooseTime = (v, disabled) => {
+    if (disabled) { return }
+    this.setState({
+      time: v
+    })
+  }
+
+  chooseTimeInt = (v, disabled) => {
+    if (disabled) { return }
+    this.setState({
+      timeInt: v
+    })
+  }
+
+  selectDate = (item) => {
+    this.setState({
+      date: item.date,
+      dateFmt: item.dateFmt,
+      time: '',
+      timeInt: ''
+    })
+  }
+
+  handleSubmit = () => {
+    const { storeId, petId, dateFmt, time, service } = this.state
+    if (!time) {
+      return Taro.showToast({
+        title: '请选择时间',
+        icon: 'none'
+      })
+    }
+    if (!petId) {
+      return Taro.showToast({
+        title: '请选择爱宠',
+        icon: 'none'
+      })
+    }
+    if (!service) {
+      return Taro.showToast({
+        title: '请选择服务项目',
+        icon: 'none'
+      })
+    }
+    const params = {
+      storeId, petId, service, reserveTime: `${dateFmt} ${time}`
+    }
+    console.log('>>> 下单参数', params)
+    Taro.showToast({
+      title: '敬请期待',
+      icon: 'none'
+    })
+  }
+
   todo = () => {
     Taro.showToast({
       title: '敬请期待',
@@ -84,7 +209,15 @@ class index extends Component {
   }
 
   render () {
-    const { banners, petId } = this.state
+    const { banners, petId, serviceList, service, timeList, timeInt, time, dateList, date, dateFmt } = this.state
+    const subTime = timeInt
+      ? [
+          timeInt.slice(0, 3) + '00',
+          timeInt.slice(0, 3) + '15',
+          timeInt.slice(0, 3) + '30',
+          timeInt.slice(0, 3) + '45'
+        ]
+      : []
     const { pet } = this.props
     const petList = _.get(pet, 'list', [])
     const currentPet = petList.filter((item) => item.id == petId)[0] || ''
@@ -145,6 +278,129 @@ class index extends Component {
           </View>
 
           {currentPet && <View className='u-pet__bottom'>{currentPet.petName}</View>}
+        </View>
+
+        <View className='u-service'>
+          <View className='u-service__label'>服务套餐</View>
+          <ScrollView scrollX>
+            <View className='u-service__list'>
+              {serviceList.map((item) => {
+                return (
+                  <View key={item.name} className='u-service__item' onClick={this.selectedService.bind(this, item)}>
+                    <View className='u-service__header'>
+                      <Image className='u-service__img' src={config.petAvatar} />
+                      {item.name == service && (
+                        <View className='u-service__selected'>
+                          <Iconfont type='iconxuanzhong' color='#fff' size='16' />
+                        </View>
+                      )}
+                    </View>
+                    <View className='u-service__name'>{item.name}</View>
+                  </View>
+                )
+              })}
+            </View>
+          </ScrollView>
+          <View className='u-service__detail'>
+            <View>查看详情</View>
+            <Iconfont type='iconarrowright' color='#ccc' size='16' />
+          </View>
+        </View>
+
+        <View className='u-time'>
+          <View className='u-time__label'>选择时间</View>
+          <View className='u-date'>
+            <ScrollView scrollX>
+              <View className='u-date__list'>
+                {dateList.map((item, x) => {
+                  return (
+                    <View
+                      className={classNames({
+                        'u-date__item': true,
+                        'u-date__active': item.date === date
+                      })}
+                      key={item.date}
+                      onClick={this.selectDate.bind(this, item)}
+                    >
+                      {x === 0 && (
+                        <View className='u-date__short'>
+                          <Text>今天</Text>
+                        </View>
+                      )}
+                      {x !== 0 && (
+                        <Block>
+                          <View className='u-date__long'>{item.date}</View>
+                          <View className='u-date__short'>{item.week}</View>
+                        </Block>
+                      )}
+                    </View>
+                  )
+                })}
+              </View>
+            </ScrollView>
+          </View>
+          {timeList.map((item) => (
+            <View key={JSON.stringify(item)}>
+              <View className='u-time__list'>
+                {item.map((v) => {
+                  const disabled = isDisabled(dateFmt, v)
+                  return (
+                    <View
+                      className={classNames({
+                        'u-time__item': true,
+                        'u-time__active': v == timeInt,
+                        'u-time__disabled': disabled
+                      })}
+                      key={v}
+                      onClick={this.chooseTimeInt.bind(this, v, disabled)}
+                    >
+                      {v}
+                    </View>
+                  )
+                })}
+              </View>
+              {item.includes(timeInt) && (
+                <View className='u-time__subList'>
+                  {subTime.map((v) => {
+                    const disabled = isDisabled(dateFmt, v)
+                    return (
+                      <View
+                        className={classNames({
+                          'u-time__subItem': true,
+                          'u-time__subActive': v == time,
+                          'u-time__disabled': disabled
+                        })}
+                        key={v}
+                        onClick={this.chooseTime.bind(this, v, disabled)}
+                      >
+                        {v}
+                        {v == time && (
+                          <View className='u-time__selected'>
+                            <Iconfont type='iconxuanzhong' color='#fff' size='14' />
+                          </View>
+                        )}
+                      </View>
+                    )
+                  })}
+                </View>
+              )}
+            </View>
+          ))}
+
+          <View className='u-time__tip'>*迟到15分钟以上，门店不保留预约资格，请合理安排时间</View>
+        </View>
+
+        <View className='u-action'>
+          <View className='u-action__left'></View>
+          <View className='u-action__right'>
+              <View>
+                <Text className='u-action__label'>合计：</Text>
+                <Text className='u-action__val'>¥ 100.00</Text>
+              </View>
+              <AtButton className='u-action__btn' type='primary' circle={false} full onClick={this.handleSubmit}>
+                去下单
+              </AtButton>
+            </View>
         </View>
       </View>
     )
